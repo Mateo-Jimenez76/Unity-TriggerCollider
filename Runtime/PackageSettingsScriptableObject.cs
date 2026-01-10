9b6b39b3534822157040c4682eca13cb535faf47
Mateo-Jimenez76 <185623516+Mateo-Jimenez76@users.noreply.github.com>
2026-01-09 18:39:46 -0800

    chore(CustomSettings): Deleted old PackageSettingsSO

--- Runtime/PackageSettingsScriptableObject.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PackageSettings", menuName = "SimpleTriggerColliders/Package Settings Scriptable Object")]
-public class PackageSettingsScriptableObject : ScriptableObject
-{
-    [Tooltip("If true, enables debug logging for the package.")]
-    public bool enableDebugLogging = false;
-}
